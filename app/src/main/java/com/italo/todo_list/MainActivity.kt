0b0e586876d6a8e52e4cf828135c49161c13f624
Italo Melo <italord.melo@gmail.com>
2021-08-07 16:16:41 -0300

    Initial commit

---
+++ app/src/main/java/com/italo/todo_list/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.italo.todo_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.italo.todo_list.ui.theme.TodolistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodolistTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello my $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodolistTheme {
+        Greeting("Android")
+    }
+}
